847207544b7fb00cb4c90553ebfd785c28e8388f
LuisFerRodVar <luisfrv1@estudiantec.cr>
2025-03-23 00:05:31 -0600

    Added main menu view

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import {MatTooltipModule} from '@angular/material/tooltip'
 
 
 @Component({
   selector: 'app-main-menu',
-  imports: [MatCardModule, MatTooltipModule],
+  imports: [MatCardModule, MatTooltipModule, MatButtonModule],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.css'
 })
